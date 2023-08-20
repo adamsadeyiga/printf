a60c9413c387d16f4b89595f21e5be012a538167
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-08-20 16:26:27 +0100

    More betty checks

--- write_handlers.c
+++ write_handlers.c
@@ -107,7 +107,6 @@ int write_num(int ind, char buffer[],
 	      int length, char padd, char extra_c)
 
 {
-
 	int i, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
@@ -125,23 +124,20 @@ int write_num(int ind, char buffer[],
 		for (i = 1; i < width - length + 1; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-		/* add extra char to left of buffer */
 		if (flags & F_MINUS && padd == ' ')
-		{
+		{ /* add extra char to left of buffer */
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
-		/* add extra char to left of buff */
 		else if (!(flags & F_MINUS) && padd == ' ')
-		{
+		{ /* add extra char to left of buff */
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
 		}
-		/* add extra char to left of padd */
 		else if (!(flags & F_MINUS) && padd == '0')
-		{
+		{ /* add extra char to left of padd */
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
 			return (write(1, &buffer[padd_start], i - padd_start) +
@@ -228,7 +224,6 @@ int write_pointer(char buffer[], int ind, int length,
 		  int width, int flags, char padd, char extra_c, int padd_start)
 
 {
-
 	int i;
 
 	if (width > length)
@@ -236,27 +231,24 @@ int write_pointer(char buffer[], int ind, int length,
 		for (i = 3; i < width - length + 3; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-		/* add extra char to left of buffer */
 		if (flags & F_MINUS && padd == ' ')
-		{
+		{ /* add extra char to left of buffer */
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
 		}
-		/* add extra char to left of buffer */
 		else if (!(flags & F_MINUS) && padd == ' ')
-		{
+		{ /* add extra char to left of buffer */
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
 		}
-		/* add extra char to left of padd */
 		else if (!(flags & F_MINUS) && padd == '0')
-		{
+		{ /* add extra char to left of padd */
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
 			buffer[1] = '0';
