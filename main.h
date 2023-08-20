a0f86ec55ccbaef7b99133f8dac5fa0da98c6593
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-08-20 16:32:49 +0100

    More betty checks

--- main.h
+++ main.h
@@ -31,15 +31,10 @@
 #define S_SHORT 1
 
 /**
-
  * struct fmt - Struct op
-
  *
-
  * @fmt: The format.
-
  * @fn: The function associated.
-
  */
 
 struct fmt
@@ -52,15 +47,10 @@ struct fmt
 };
 
 /**
-
  * typedef struct fmt fmt_t - Struct op
-
  *
-
  * @fmt: The format.
-
  * @fm_t: The function associated.
-
  */
 
 typedef struct fmt fmt_t;
@@ -117,7 +107,8 @@ int print_hexa_upper(va_list types, char buffer[],
 
 int print_hexa(va_list types, char map_to[],
 
-	       char buffer[], int flags, char flag_ch, int width, int precision, int size);
+	       char buffer[], int flags, char flag_ch,
+	       int width, int precision, int size);
 
 /* Function to print non printable characters */
 
