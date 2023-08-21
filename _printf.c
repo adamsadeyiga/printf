19ed8b1454ab53779482fa0c785e7cd1bcf33e83
De-sam <samsoncoded@gmail.com>
2023-08-21 11:49:43 +0100

    updated some variables

--- _printf.c
+++ _printf.c
@@ -1,98 +1,68 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void print_buffer(char buffer[], int *buffIndex);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of printed characters
  */
-
 int _printf(const char *format, ...)
-
 {
+    int currentIndex, totalPrinted = 0, totalPrintedChars = 0;
+    int formatFlags, fieldWidth, precisionValue, dataSize, bufferIndex = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
-	int i, printed = 0, printed_chars = 0;
+    if (format == NULL)
+        return (-1);
 
-	int flags, width, precision, size, buff_ind = 0;
+    va_start(list, format);
 
-	va_list list;
+    for (currentIndex = 0; format && format[currentIndex] != '\0'; currentIndex++)
+    {
+        if (format[currentIndex] != '%')
+        {
+            buffer[bufferIndex++] = format[currentIndex];
+            if (bufferIndex == BUFF_SIZE)
+                print_buffer(buffer, &bufferIndex);
+            totalPrintedChars++;
+        }
+        else
+        {
+            print_buffer(buffer, &bufferIndex);
 
-	char buffer[BUFF_SIZE];
+            formatFlags = get_flags(format, &currentIndex);
+            fieldWidth = get_width(format, &currentIndex, list);
+            precisionValue = get_precision(format, &currentIndex, list);
+            dataSize = get_size(format, &currentIndex);
+            currentIndex++;
 
-	if (format == NULL)
+            totalPrinted = handle_print(format, &currentIndex, list, buffer,
+                                         formatFlags, fieldWidth, precisionValue, dataSize);
 
-		return (-1);
+            if (totalPrinted == -1)
+                return (-1);
 
-	va_start(list, format);
+            totalPrintedChars += totalPrinted;
+        }
+    }
 
-	for (i = 0; format && format[i] != '\0'; i++)
+    print_buffer(buffer, &bufferIndex);
+    va_end(list);
 
-	{
-
-		if (format[i] != '%')
-
-		{
-
-			buffer[buff_ind++] = format[i];
-
-			if (buff_ind == BUFF_SIZE)
-
-				print_buffer(buffer, &buff_ind);
-
-			/* write(1, &format[i], 1);*/
-
-			printed_chars++;
-		}
-
-		else
-
-		{
-
-			print_buffer(buffer, &buff_ind);
-
-			flags = get_flags(format, &i);
-
-			width = get_width(format, &i, list);
-
-			precision = get_precision(format, &i, list);
-
-			size = get_size(format, &i);
-
-			++i;
-
-			printed = handle_print(format, &i, list, buffer,
-
-					       flags, width, precision, size);
-
-			if (printed == -1)
-
-				return (-1);
-
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+    return (totalPrintedChars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of characters
+ * @buffIndex: Pointer to the buffer index, represents the length
  */
-
-void print_buffer(char buffer[], int *buff_ind)
-
+void print_buffer(char buffer[], int *buffIndex)
 {
+    if (*buffIndex > 0)
+        write(1, &buffer[0], *buffIndex);
 
-	if (*buff_ind > 0)
-
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+    *buffIndex = 0;
 }
