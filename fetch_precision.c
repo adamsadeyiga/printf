4bc2d8628241c9925030f19b60e99dff677935d8
De-sam <samsoncoded@gmail.com>
2023-08-22 16:21:00 +0100

    update: fixing output failure

--- fetch_precision.c
+++ fetch_precision.c
@@ -17,36 +17,45 @@
  */
 
 int fetch_precision(const char *format, int *i, va_list list)
+
 {
+
 	int curr_i = *i + 1;
+
 	int precision = -1;
 
 	if (format[curr_i] != '.')
-		return precision;
+
+		return (precision);
 
 	precision = 0;
-	curr_i++;
 
-	while (format[curr_i] != '\0')
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
+
 		if (is_digit(format[curr_i]))
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-			curr_i++;
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-		{
-			break;
-		}
-	}
 
+			precision *= 10;
+
+			precision += format[curr_i] - '0';
+		}
+
+		else if (format[curr_i] == '*')
+
+		{
+
+			curr_i++;
+
+			precision = va_arg(list, int);
+
+			break;
+		}
+
+		else
+
+			break;
+	}
 	*i = curr_i - 1;
-	return precision;
+	return (precision);
 }
--- output_handler.c
+++ output_handler.c
@@ -21,21 +21,47 @@
  */
 
 int output_handler(const char *fmt, int *ind, va_list list, char buffer[],
-		   int flags, int width, int precision, int size)
+
+		 int flags, int width, int precision, int size)
 {
-	int i, printed_chars = 0;
+
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-	    {'c', print_char}, {'s', print_string}, {'%', print_percent}, {'i', print_int}, {'d', print_int}, {'b', print_binary}, {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal}, {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable}, {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}};
-
+	    {'c', print_char}, {'s', print_string}, {'%', print_percent},
+	    {'i', print_int},
+	    {'d', print_int},
+	    {'b', print_binary},
+	    {'u', print_unsigned},
+	    {'o', print_octal},
+	    {'x', print_hexadecimal},
+	    {'X', print_hexa_upper},
+	    {'p', print_pointer},
+	    {'S', print_non_printable},
+	    {'r', print_reverse},
+	    {'R', print_rot13string},
+	    {'\0', NULL}
+	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-	{
 		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
 		{
-			return fmt_types[i].fn(list, buffer, flags, width, precision, size);
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
 		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-
-	// Handle unknown format specifier here if needed
-
-	return printed_chars;
+	return (printed_chars);
 }
