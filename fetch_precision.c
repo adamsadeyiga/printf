e1735d5d7d23a5eb269df21651a2a8e5aa859a4b
De-sam <samsoncoded@gmail.com>
2023-08-22 16:19:11 +0100

    update: fixing output failure

--- fetch_precision.c
+++ fetch_precision.c
@@ -17,45 +17,36 @@
  */
 
 int fetch_precision(const char *format, int *i, va_list list)
-
 {
-
 	int curr_i = *i + 1;
-
 	int precision = -1;
 
 	if (format[curr_i] != '.')
-
-		return (precision);
+		return precision;
 
 	precision = 0;
+	curr_i++;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	while (format[curr_i] != '\0')
 	{
-
 		if (is_digit(format[curr_i]))
 		{
-
 			precision *= 10;
-
 			precision += format[curr_i] - '0';
-		}
-
-		else if (format[curr_i] == '*')
-
-		{
-
 			curr_i++;
-
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
 			precision = va_arg(list, int);
-
 			break;
 		}
-
 		else
-
+		{
 			break;
+		}
 	}
+
 	*i = curr_i - 1;
-	return (precision);
+	return precision;
 }
