536b7a20e3cfe5a20060c36200aba4e23076c481
De-sam <samsoncoded@gmail.com>
2023-08-22 12:50:48 +0100

    fixing commit error

--- _printf.c
+++ _printf.c
@@ -1,15 +1,14 @@
-/**
-*Hey there, code voyagers! 🚀📄
-*Feast your eyes upon the creation of brilliance!
-*Behold, the file '_printf.c'
-*Authored with love and expertise by
-*Adams Adeyiga and Odunaro Samson! 💻❤️
-*Remember, folks: when these two unite,
-*code magic takes flight! 🌟✨
-*/
+/***
+ *Hey there, code voyagers! 🚀📄
+ *Feast your eyes upon the creation of brilliance!
+ *Behold, the file '_printf.c'
+ *Authored with love and expertise by
+ *Adams Adeyiga and Odunaro Samson! 💻❤️
+ *Remember, folks: when these two unite,
+ *code magic takes flight! 🌟✨
+ */
 
 #include "main.h"
-
 
 /**
  * _printf - Custom printf function
@@ -18,49 +17,49 @@
  */
 int _printf(const char *format, ...)
 {
-    int currentIndex, totalPrinted = 0, totalPrintedChars = 0;
-    int formatFlags, fieldWidth, precisionValue, dataSize, bufferIndex = 0;
-    va_list list;
-    char buffer[BUFF_SIZE];
+	int currentIndex, totalPrinted = 0, totalPrintedChars = 0;
+	int formatFlags, fieldWidth, precisionValue, dataSize, bufferIndex = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(list, format);
+	va_start(list, format);
 
-    for (currentIndex = 0; format && format[currentIndex] != '\0'; currentIndex++)
-    {
-        if (format[currentIndex] != '%')
-        {
-            buffer[bufferIndex++] = format[currentIndex];
-            if (bufferIndex == BUFF_SIZE)
-                print_buffer(buffer, &bufferIndex);
-            totalPrintedChars++;
-        }
-        else
-        {
-            print_buffer(buffer, &bufferIndex);
+	for (currentIndex = 0; format && format[currentIndex] != '\0'; currentIndex++)
+	{
+		if (format[currentIndex] != '%')
+		{
+			buffer[bufferIndex++] = format[currentIndex];
+			if (bufferIndex == BUFF_SIZE)
+				print_buffer(buffer, &bufferIndex);
+			totalPrintedChars++;
+		}
+		else
+		{
+			print_buffer(buffer, &bufferIndex);
 
-            formatFlags = fetch_flags(format, &currentIndex);
-            fieldWidth = fetch_width(format, &currentIndex, list);
-            precisionValue = fetch_precision(format, &currentIndex, list);
-            dataSize = fetch_size(format, &currentIndex);
-            currentIndex++;
+			formatFlags = fetch_flags(format, &currentIndex);
+			fieldWidth = fetch_width(format, &currentIndex, list);
+			precisionValue = fetch_precision(format, &currentIndex, list);
+			dataSize = fetch_size(format, &currentIndex);
+			currentIndex++;
 
-            totalPrinted = output_handler(format, &currentIndex, list, buffer,
-                                         formatFlags, fieldWidth, precisionValue, dataSize);
+			totalPrinted = output_handler(format, &currentIndex, list, buffer,
+						      formatFlags, fieldWidth, precisionValue, dataSize);
 
-            if (totalPrinted == -1)
-                return (-1);
+			if (totalPrinted == -1)
+				return (-1);
 
-            totalPrintedChars += totalPrinted;
-        }
-    }
+			totalPrintedChars += totalPrinted;
+		}
+	}
 
-    print_buffer(buffer, &bufferIndex);
-    va_end(list);
+	print_buffer(buffer, &bufferIndex);
+	va_end(list);
 
-    return (totalPrintedChars);
+	return (totalPrintedChars);
 }
 
 /**
@@ -70,8 +69,8 @@ int _printf(const char *format, ...)
  */
 void print_buffer(char buffer[], int *buffIndex)
 {
-    if (*buffIndex > 0)
-        write(1, &buffer[0], *buffIndex);
+	if (*buffIndex > 0)
+		write(1, &buffer[0], *buffIndex);
 
-    *buffIndex = 0;
+	*buffIndex = 0;
 }
